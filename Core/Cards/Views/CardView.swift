ac17952540556fb48064ad40bd88e53fada2863b
Fatih Eren Özçelik <f.eren.ozcelik@gmail.com>
2024-03-14 10:01:44 +0300

    Card swipe feature added

--- Core/Cards/Views/CardView.swift
+++ Core/Cards/Views/CardView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct CardView: View {
     
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: Double = 0
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Image(.shakira1)
@@ -20,6 +23,14 @@ struct CardView: View {
         }
         .frame(width: cardWidth, height: cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged(_:))
+                .onEnded(onDragEnded(_:))
+        )
     }
 }
 
@@ -37,4 +48,21 @@ private extension CardView {
         UIScreen.main.bounds.height / 1.45
     }
     
+    var screenCutoff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
+    }
+    
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+        
+        if abs(width) <= abs(screenCutoff) {
+            xOffset = 0
+            degrees = 0
+        }
+    }
 }
